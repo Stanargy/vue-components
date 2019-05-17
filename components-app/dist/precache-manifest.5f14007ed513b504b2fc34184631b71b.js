0c48abc74bbf2b72c2b4e95efd7506fff4b88373
Stan Ooms <35112551+Stanargy@users.noreply.github.com>
2019-05-17 19:29:09 +0200

    init

---
+++ components-app/dist/precache-manifest.5f14007ed513b504b2fc34184631b71b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "13c7d5d6baf417ddc493",
+    "url": "/js/chunk-vendors.fe87fe13.js"
+  },
+  {
+    "revision": "cddb598ff1ad04855080",
+    "url": "/js/app.5029660c.js"
+  },
+  {
+    "revision": "69a8e6b0509e08aec110",
+    "url": "/js/about.903e2c29.js"
+  },
+  {
+    "revision": "230bc3359b64926c702bb0bcfc643db8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cddb598ff1ad04855080",
+    "url": "/css/app.a96b6da2.css"
+  },
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  }
+];
